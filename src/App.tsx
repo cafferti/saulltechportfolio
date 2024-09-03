00d86cccd67cdb50e105736be7ed5139bca78e48
saulltech <saulkumven@gmail.com>
2024-09-03 12:41:58 +0100

    replace react set up with a more functional version

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-function App() {
-  const [count, setCount] = useState(0)
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './index.css'
 
+
+function App() {
   return (
-    <>
+    <div className="bg-black text-white border-l-8">
       <div className='bg-black'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        helllo css world good
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    fontFamily: {
-      'pops': ['Poppins', 'sans-serif'],
-      'robo': ['Roboto', 'sans-serif'],
-      'inter': ['Inter', 'sans-serif'],
-    }
   },
   plugins: [],
 }
